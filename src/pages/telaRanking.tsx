efdfb89ba0cbbae6485a0d27f96473056a568ceb
juliaRobertav <juguirald@gmail.com>
2025-05-12 16:46:42 -0300

    ranking page done

--- src/pages/telaRanking.tsx
+++ src/pages/telaRanking.tsx
@@ -1,9 +1,42 @@
+import { useEffect, useState } from 'react';
 import './../styles/telaRanking.css'
 
+interface RankingItem {
+    nome: string;
+    score: number
+}
+
 function Ranking() {
+
+    const [ranking, setRanking] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        console.log("entrou useEddect")
+        fetch("http://localhost:8000/ranking")
+            .then((response) => response.json())
+            .then((data) => {
+                setRanking(data.ranking);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error("Erro ao buscar ranking:", error);
+                setLoading(false);
+            });
+    }, []);
+
+
     return (
         <div className="container_ranking">
-            <h1>Ranking</h1>
+            <h1 className='ranking_titulo'>Ranking</h1>
+            <div>
+                {ranking.map((item: RankingItem) => (
+                    <div className='ranking_item'>
+                        <h1>{item.nome}</h1>
+                        <h1>{item.score}</h1>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
